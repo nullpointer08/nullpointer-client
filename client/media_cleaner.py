2b73403b248f23d329be2036b582b1ec17f6710f
Hannu Lahtinen <hannu.lahtinen@student.tut.fi>
2015-12-22 00:56:07 +0200

    asdf

--- client/media_cleaner.py
+++ client/media_cleaner.py
@@ -31,7 +31,7 @@ class MediaCleaner(object):
 
     def clean_media(self, content_length):
         if not self.enough_space(content_length):
-                self.run_cleanup(content_length)
+            self.run_cleanup(content_length)
 
     def run_cleanup(self, content_length):
         self.LOG.debug('Cleaning up old media.')
